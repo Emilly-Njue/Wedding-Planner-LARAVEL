042743bcf63f6b17d43c0740a84e614edf08818f
Emilly Njue <muruginjue627@gmail.com>
2023-12-29 14:49:37 +0300

    seating charts

--- resources/views/services/seating.blade.php
+++ resources/views/services/seating.blade.php
@@ -96,30 +96,31 @@
                     <div class="col-lg-8 content-area">
                         <div class="ttm-service-single-content-area">
                             <div class="ttm-featured-wrapper mb-35 res-991-mb-20">
-                                <img class="img-fluid" src="images/services/services-04.jpg" alt="">
+                                <img class="img-fluid" src="{{asset('images/services/004.jpeg')}}" alt="">
                             </div>
-                            <h4>Venue and Stage Decorated</h4>
+                            <h4 style="color: rgb(212, 254, 255); font-size:20px; font-family:'Croissant One', 'Great Vibes', 'Lobster', 'Roboto', 'Rubik'; font-weight: 50;">Table Tales: Where Love Takes a Seat:</h4>
                             <div class="mb-35">
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,&nbsp;<strong>eaque ipsa quae ab illo inventore</strong>&nbsp;veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+                                <p class="white-text">Forget spreadsheets and awkward shuffles. Wedco's interactive seating chart tool transforms the dreaded task into a delightful exercise in storytelling. Imagine weaving your guests into the fabric of your celebration, placing them like precious pearls on a glittering necklace, ensuring every seat sparks laughter, conversation, and memories that last a lifetime.</p>
+                                <p class="white-text"><strong>We believe seating isn't just logistics, it's storytelling.</strong> Let us weave your guest list into a masterpiece, ensuring every seat sparks conversation, ignites laughter, and becomes a cherished part of your wedding celebration.</p>
                             </div>
                             <div class="ttm-featured-wrapper mb-35 res-991-mb-20">
-                                <img class="img-fluid" src="images/services/services-07.jpg" alt="">
+                                <img class="img-fluid" src="{{asset('images/services/seats-07.jpeg')}}" alt="">
                             </div>
-                            <h4 class="mt-15">Indoor Air Quality</h4>
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop.</p>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <h4 class="mt-15" style="color: rgb(212, 254, 255); font-size:20px; font-family:'Croissant One', 'Great Vibes', 'Lobster', 'Roboto', 'Rubik'; font-weight: 50;">Cheers to Clinking & Connections: Seating Made Simple</h4>
+                                <p class="white-text"><strong>Ditch the stress, embrace the fun!</strong> Wedco's intuitive tools take the headache out of seating arrangements. Upload your guest list, browse pre-designed templates, or work with our experts to create a chart that reflects your vision.</p>
+                                <p class="white-text"><strong>No more late-night Excel struggles.</strong> We handle RSVPs, dietary restrictions, family dynamics, and even sprinkle in surprise tablemates to encourage mingling and laughter.</p>
+                                <p class="white-text"><strong>Sit back, relax, and let Wedco work its magic.</strong> We'll deliver a visually stunning, easy-to-navigate seating chart that sets the stage for a night of connection and celebration.</p>
                                 <div class="ttm-horizontal_sep width-100 mt-35 mb-25"></div>
                             <div class="row">
                                 <div class="col-sm-6">
-                                    <h4>Commercial Planning</h4>
-                                    <p>Sed ut perspiciatis unde omnis iste natus error sit, totam rem aperiam,&nbsp;<strong>eaque ipsa quae ab illo inventore</strong>veritatis etqo quasi architecto beatae vitae dicta sunt explicabo&nbsp; Nemo enimjq Letraset sheets containing Lorem Ipsum.</p>
-                                    <p>Omnis iste natus error sit voluptatem accusantium doloremque laudantium, but the majority suffered.</p>
+                                    <h4 style="color: rgb(212, 254, 255); font-size:20px; font-family:'Croissant One', 'Great Vibes', 'Lobster', 'Roboto', 'Rubik'; font-weight: 50;">Love & Laughter at Every Place Setting: Wedco's Seating Secrets</h4>
+                                    <p class="white-text"><strong>Go beyond assigned seats.</strong> We believe in creating interactive experiences that spark joy and conversation. Think mix-and-mingle cocktail hour rounds, open seating spaces for spontaneous connections, and designated dance floor pods for epic group grooves.</p>
+                                    <p class="white-text"><strong>From seating whispers to party roars, Wedco orchestrates the flow.</strong> We make sure every guest feels included, comfortable, and ready to create unforgettable memories around the table and beyond.</p>
                                     <a class="ttm-btn ttm-btn-size-md ttm-btn-shape-rounded ttm-btn-style-border ttm-icon-btn-right ttm-btn-color-darkgrey" href="#" title="" tabindex="0">View More</a>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="mt-10 res-991-mt-20 ttm_single_image-wrapper">
-                                        <img class="img-fluid" src="images/services/services-06.jpg" alt="image"> 
+                                        <img class="img-fluid" src="{{asset('images/services/seats-06.jpeg')}}" alt="image"> 
                                     </div>
                                 </div>
                             </div>
